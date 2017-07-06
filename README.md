13a1958bcb2cba83cbdfa02585b73185273b99b5
Wei Xie <wei.xie@citrix.com>
2017-07-06 15:30:44 +0800

    Add readme and add auto installation
    Signed-off-by: Wei Xie <wei.xie@citrix.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+
+- yum install wxpython
+- pip install git+https://github.com/xiewei20082008/xs-tools.git
+
--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 
 setup(name='xs-tools',
       version='0.1.0',
-      packages=['changelog','pin-generator'],
+      packages=['changelog','pinGenerator'],
+      install_requires = ['gooey','wget','subprocess32'],
       entry_points={
           'console_scripts': [
               'changelog = changelog.main:main',
-              'xsbuild = pin-generator.main:main'
-          ],
+			  'xsbuild = pinGenerator.main:main'
+          ]
       },
-      install_requires = ['gooey','wget','subprocess32'],  
       author = "Wei Xie",
       author_email = 'xiewei.fire@gmail.com'
 )
